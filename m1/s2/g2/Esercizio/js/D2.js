ea828236cfcf92cf531d80a164038ee65321a8b4
Skaav <scavinopaolo95@gmail.com>
2023-04-04 14:14:56 +0200

    fix

--- m1/s2/g2/Esercizio/js/D2.js
+++ m1/s2/g2/Esercizio/js/D2.js
@@ -10,7 +10,7 @@ REGOLE
 /* ESERCIZIO 1
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
 */
-//Fare esempi di ogni tipo di datatype in JavaScript e usare il typeoff per farlo vedere, aggiungere anche un collegamento che lo rimandi a schermo nel main.
+
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 /* ESERCIZIO 2
@@ -23,16 +23,16 @@ console.log(name);
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 //esistono due modi, ho diamo a due variabili i valori 12 e 20 e poi li sommiamo, o chiediamo un console.log() in cui andiamo a scrivere l'operazione matematica richiesta
-var k = 12;
-var l = 20;
+let k = 12;
+let l = 20;
 console.log(k + l);
-console.log(12+20);
+console.log(12 + 20);
 
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
-var x = 12;
+let x = 12;
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
@@ -58,8 +58,7 @@ console.log(4 - x);
  Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
-var name1 = "jhon";
+var name1 = "john";
 var name2 = "John";
-
-console.log(name1==name2);
-
+console.log(name1 == name2);
+console.log(name1.toLowerCase === name2.toLowerCase);
