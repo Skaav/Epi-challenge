9c9fc3053385de61a8263c4dd0cc13963074c1e2
Skaav <scavinopaolo95@gmail.com>
2023-04-12 09:44:29 +0200

    fix

--- m1/s3/g1/date.js
+++ m1/s3/g1/date.js
@@ -3,3 +3,7 @@ let now = new Date();
 
 // Già visto con Boolean(); String(); La maiuscola indica un Oggeto
 
+/*
+Si prende a livello del server la data UTC e solo in fase di visualizzazione
+si calcola il fuso orario 
+*/
