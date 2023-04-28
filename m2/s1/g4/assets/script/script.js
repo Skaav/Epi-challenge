e71cc399b4663b942edf9fbb7318adb14de527cd
Skaav <scavinopaolo95@gmail.com>
2023-04-28 12:50:19 +0200

    media

---
+++ m2/s1/g4/assets/script/script.js
@@ -0,0 +1,32 @@
+let navbar = document.querySelector('nav');
+let button = document.querySelector('.start');
+
+window.onscroll = function () {
+    if (window.pageYOffset > 200) {
+        navbar.classList.add('colorSwitch');
+        button.classList.add('colorSwitch2');
+    } else {
+        navbar.classList.remove('colorSwitch')
+        button.classList.remove('colorSwitch2')
+    }
+};
+
+// ANIMAZIONE LETTERE
+
+let letters = document.querySelectorAll('.artwork svg g')
+
+function selectRandom(x) {
+    return x[Math.floor(Math.random() * x.length)];
+}
+
+// aggiungo classe per farle sparire
+function lettereAnimate() {
+    let switchTrue = selectRandom(Array.from(letters));
+    if (switchTrue.classList.contains('letterM')) {
+        switchTrue.classList.remove('letterM');
+    } else {
+        switchTrue.classList.add('letterM');
+    }
+};
+//attivo tutto
+setInterval(lettereAnimate, 2);
