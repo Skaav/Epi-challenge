d0c5f7644566f564c11ad475b9d52d301358c570
Skaav <scavinopaolo95@gmail.com>
2023-05-09 16:45:08 +0200

    fixxi

--- m2/s3/g2/index.js
+++ m2/s3/g2/index.js
@@ -1,44 +1,37 @@
 const savedName = localStorage.getItem("name");
 if (savedName) {
-    document.getElementById("saved-name").textContent = "Nome salvato: " + savedName;
+    document.querySelector("#savedName").textContent = "Nome salvato: " + savedName;
 }
 
-document.getElementById("save-btn").addEventListener("click", function () {
-    const name = document.getElementById("name").value;
+document.querySelector("#saveBtn").addEventListener("click", function () {
+    const name = document.querySelector("#name").value;
     if (name) {
         localStorage.setItem("name", name);
-        document.getElementById("saved-name").textContent = "Nome salvato: " + name;
+        document.querySelector("#savedName").textContent = "Nome salvato: " + name;
     }
 });
 
-document.getElementById("remove-btn").addEventListener("click", function () {
+document.querySelector("#removeBtn").addEventListener("click", function () {
     localStorage.removeItem("name");
-    document.getElementById("name").value = "";
-    document.getElementById("saved-name").textContent = "";
+    document.querySelector("#name").value = "";
+    document.querySelector("#savedName").textContent = "";
 });
 
 //Contatore1
 let count = sessionStorage.getItem('count') || 0;
-const timer = document.getElementById('timer');
+const timer = document.querySelector('#timer');
 
 setInterval(() => {
     count++;
-    timer.textContent = `${count} secondi`;
+    timer.textContent = `${count}`;
     sessionStorage.setItem('count', count);
 }, 1000);
 
 //Contatore2
-if (typeof (Storage) !== "undefined") {
-    if (sessionStorage.counter) {
-        var counter = parseInt(sessionStorage.counter);
-    } else {
-        var counter = 0;
-    }
-    setInterval(function () {
-        counter++;
-        sessionStorage.counter = counter;
-        document.getElementById("counter").innerHTML = counter;
-    }, 1000);
-} else {
-    document.getElementById("counter").innerHTML = "Session storage non supportato dal browser.";
-}
+let seconds = 0;
+let reset = document.querySelector('#reset');
+
+setInterval(() => {
+    seconds++;
+    reset.textContent = `${seconds}`;
+}, 1000);
