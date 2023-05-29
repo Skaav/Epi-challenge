ead1ab88cb8a4eba48f547201d8fd7aa4f0a3a0e
Skaav <128913847+Skaav@users.noreply.github.com>
2023-05-29 16:11:53 +0200

    esercizio g1

---
+++ m3/s2/g1/eserciziog1/src/app/componente1/componente1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente1',
+  templateUrl: './componente1.component.html',
+  styleUrls: ['./componente1.component.sass']
+})
+export class Componente1Component {
+
+}
