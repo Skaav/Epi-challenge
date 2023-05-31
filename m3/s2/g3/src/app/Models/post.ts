ab74b43cccad04ad843dcd80a1a1846d6d11243f
Skaav <128913847+Skaav@users.noreply.github.com>
2023-05-31 17:01:22 +0200

    fix

---
+++ m3/s2/g3/src/app/Models/post.ts
@@ -0,0 +1,12 @@
+export class Post {
+    "id": number;
+    "title": string;
+    "active": boolean
+
+    constructor(id: number, title: string, active: boolean) {
+        this.id = id;
+        this.title = title;
+        this.active = active;
+    }
+
+}
