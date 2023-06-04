d40898004c5cb561e6838c2903125018ec2ddb7c
Skaav <128913847+Skaav@users.noreply.github.com>
2023-06-04 13:43:55 +0200

    Ultimi cambiamenti

--- m3/s2/progetto/src/app/serviceclass.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Serviceclass } from './serviceclass';
-
-describe('Serviceclass', () => {
-  it('should create an instance', () => {
-    expect(new Serviceclass()).toBeTruthy();
-  });
-});
